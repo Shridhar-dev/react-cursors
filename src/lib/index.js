6385cb94d056f930b2d3f586e7beac23bf47416e
Rajkumar-justcoder <meet.r.graphicat10@gmail.com>
2023-07-05 10:53:15 +0530

    Feat: initalized an empty repo containing only .gitignore file

--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-import CircleCursor from "./components/CircleCursor";
-import EmojiCursor from "./components/EmojiCursor";
-import ImageCursor from "./components/ImageCursor";
-
-export { CircleCursor, EmojiCursor, ImageCursor };
